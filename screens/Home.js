e1aa95b9b46fedb826cd4e36a84979010475db7d
Mark Gerken <mgerken86@gmail.com>
2022-09-01 19:11:06 -0600

    components

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
-import { View, SafeArea, FlatList, Text } from "react-native"
+import { View, SafeArea, FlatList, Text, SafeAreaView } from "react-native"
 import { COLORS, NFTData } from '../constants'
+import { NFTCard, HomeHeader, FocusedStatusBar } from "./components"
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+
+    </SafeAreaView>
   )
 }
 
