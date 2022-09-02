7754a65b9351c4c7184014351442bbc35b49056d
Mark Gerken <mgerken86@gmail.com>
2022-09-02 17:07:00 -0600

    details description

--- components/index.js
+++ components/index.js
@@ -4,9 +4,11 @@ import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
+import { SubInfo } from './SubInfo'
 
 export {
     HomeHeader, NFTCard, FocusedStatusBar,
     CircleButton, RectButton,
-    DetailsBid, DetailsDesc
+    DetailsBid, DetailsDesc,
+    SubInfo
 }
