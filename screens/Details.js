cf4f67a4e2b36125fa6c67b8953996088af39cbc
Mark Gerken <mgerken86@gmail.com>
2022-09-02 16:53:08 -0600

    circle buttons on detail header

--- screens/Details.js
+++ screens/Details.js
@@ -1,6 +1,34 @@
 import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native"
 import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants'
 import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from "../components"
+import React from "react"
+
+
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{ width: '100%', height: 373 }}>
+    <Image
+      source={data.image}
+      resizeMode='cover'
+      style={{ width: '100%', height: '100%' }}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+
+<CircleButton
+      imgUrl={assets.heart}
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
+
+  </View>
+)
+
+
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params
@@ -22,11 +50,19 @@ const Details = ({ route, navigation }) => {
         backgroundColor: 'rgba(255,255,255,.5)',
         zIndex: 1
       }}>
-        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS}/>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS} />
       </View>
-      <FlatList 
-      data ={data.bids}
-      renderItem={({item}) => <DetailsBid bid={item}/>}
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+          </React.Fragment>
+        )}
       />
     </SafeAreaView>
   )
