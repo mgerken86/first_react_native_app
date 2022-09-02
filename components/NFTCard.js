a80a84799d38225262d02258cd8f73252187c0d7
Mark Gerken <mgerken86@gmail.com>
2022-09-01 21:23:46 -0600

    subInfo components

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,6 +1,7 @@
 import { View, Image, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { CircleButton, RectButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo'
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants'
 
@@ -15,19 +16,20 @@ const NFTCard = ({ data }) => {
       ...SHADOWS.dark
     }}>
       <View style={{ width: "100%", height: 250 }}>
-        <Image 
-        source={data.image}
-        resizeMode="cover"
-        style={{
-          width: "100%",
-          height: "100%",
-          borderTopLeftRadius: SIZES.font,
-          borderTopRightRadius: SIZES.font,
-        }}
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
         />
-
-        <CircleButton imgUrl={assets.heart} right={10} top={10}/>
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
+
+      <SubInfo />
     </View>
   )
 }
