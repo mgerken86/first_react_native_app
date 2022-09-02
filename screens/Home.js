961f7981595822e443e877579d32ccedb2577e26
Mark Gerken <mgerken86@gmail.com>
2022-09-01 20:58:07 -0600

    nftCard images

--- screens/Home.js
+++ screens/Home.js
@@ -8,16 +8,27 @@ import { NFTCard, FocusedStatusBar, HomeHeader } from '../components'
 const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar background={COLORS.primary}/>
+      <FocusedStatusBar background={COLORS.primary} />
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
-          <FlatList 
-          data={NFTData}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
-          keyExtractor={(item) => item.id}
-          showsVerticalScrollIndicator={false}
-          ListHeaderComponent={<HomeHeader/>}
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
           />
+          <View style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}>
+            <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+            <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+          </View>
         </View>
       </View>
     </SafeAreaView>
