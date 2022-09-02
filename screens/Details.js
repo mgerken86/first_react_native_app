28c8ffd8b41560c5a762b05f776fab8648936fd1
Mark Gerken <mgerken86@gmail.com>
2022-09-02 16:39:28 -0600

    DetailsBid in flatlist

--- screens/Details.js
+++ screens/Details.js
@@ -6,11 +6,27 @@ const Details = ({ route, navigation }) => {
   const { data } = route.params
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <FocusedStatusBar 
-      barStyle="dark-content"
-      backgroundColor='transparent'
-      translucent={true}
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor='transparent'
+        translucent={true}
+      />
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        paddingVertical: SIZES.font,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255,255,255,.5)',
+        zIndex: 1
+      }}>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS}/>
+      </View>
+      <FlatList 
+      data ={data.bids}
+      renderItem={({item}) => <DetailsBid bid={item}/>}
       />
     </SafeAreaView>
   )
