6877d5da1b67c29fd1ecc36a20a41735a82c980c
Mark Gerken <mgerken86@gmail.com>
2022-09-01 22:22:43 -0600

    homeHeader style

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,7 +1,7 @@
 import { View, Text, Image, TextInput } from 'react-native'
 import { COLORS, FONTS, SIZES, assets } from '../constants'
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
   return (
     <View style={{
       backgroundColor: COLORS.primary,
@@ -44,6 +44,29 @@ const HomeHeader = () => {
         }}>
           Let's find a masterpiece
         </Text>
+      </View>
+
+      <View style={{ marginTop: SIZES.font}}>
+        <View style={{
+          width: '100%',
+          borderRadius: SIZES.font,
+          backgroundColor: COLORS.gray,
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: SIZES.font,
+          paddingVertical: SIZES.small -2
+        }}>
+          <Image 
+          source={assets.search}
+          resizeMode='contain'
+          style={{ width: 20, height: 20, marginRight: SIZES.base }}
+          />
+          <TextInput 
+          placeholder='Search NFTs'
+          style={{ flex: 1 }}
+          onChangeText={() => {}}
+          />
+        </View>
       </View>
 
     </View>
